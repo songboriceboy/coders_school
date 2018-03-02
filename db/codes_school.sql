61d2d55ff6f4a21a325d9e2ac5886e81d317b8aa
songbo <songbo@neusoft.com>
2018-03-02 21:44:17 +0800

    update db

--- db/codes_school.sql
+++ db/codes_school.sql
@@ -1,7 +1,8 @@
 create table taginfo(
     tag_id int not null PRIMARY KEY auto_increment,
 		tag_name varchar(50) not null DEFAULT '',
-		tag_desc varchar(600) not null DEFAULT ''
+		tag_desc varchar(600) not null DEFAULT '',
+		tag_logo_path varchar(300) not null DEFAULT ''
 )engine = innodb;
 create table admininfo(
     admin_id int not null PRIMARY KEY auto_increment,
@@ -14,7 +15,17 @@ create table userinfo(
 		user_pwd varchar(64) not null DEFAULT '',
 		user_phone varchar(20) not null DEFAULT '',
 		user_avatar varchar(200) not null DEFAULT '',
-		user_score int not null DEFAULT 0
+		user_score int not null DEFAULT 0,
+		user_nicker varchar(64) not null DEFAULT '',
+		user_job varchar(64) not null DEFAULT '',
+		user_company varchar(100) not null DEFAULT '',
+		user_intro varchar(200) not null DEFAULT '',
+		user_site varchar(200) not null DEFAULT '',
+		user_email varchar(64) not null DEFAULT '',
+		user_github varchar(100) not null DEFAULT '',
+		user_weixin varchar(100) not null DEFAULT '',
+		user_qq varchar(100) not null DEFAULT '',
+		user_sina_weibo varchar(100) not null DEFAULT ''
 )engine = innodb;
 create table section_info(
     section_id int not null PRIMARY KEY auto_increment,
@@ -22,6 +33,8 @@ create table section_info(
 		section_desc varchar(600) not null DEFAULT '',
 		section_parent_id int not null DEFAULT -1
 )engine = innodb;
+
+
 
 insert into section_info(section_name,section_desc,section_parent_id)
 values('问与答','问与答',-1);
@@ -49,4 +62,31 @@ CREATE TABLE topic_comment_info (
   user_id int(11) NOT NULL DEFAULT '-1',
   PRIMARY KEY (comment_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table tag_topic_info
+(
+	tag_topic_info_id int not null PRIMARY KEY auto_increment,
+	topic_id int not null default 0,
+	tag_id int not null default 0
+)engine = innodb;
+
+create table tag_user_info
+(
+	tag_user_info_id int not null PRIMARY KEY auto_increment,
+	user_id int not null default 0,
+	tag_id int not null default 0
+)engine = innodb;
+
+create table follow_info
+(
+	follow_info_id int not null PRIMARY KEY auto_increment,
+	follower_id int not null default 0,-- 粉丝
+	followee_id int not null default 0 -- 被关注者
+)engine = innodb;
+create table user_topic_love
+(
+	user_topic_love_id int not null PRIMARY KEY auto_increment,
+	user_id int not null default 0,-- 粉丝
+	topic_id int not null default 0 -- 被关注者
+)engine = innodb;
 
